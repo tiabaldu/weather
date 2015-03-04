203f3616c56adeda80f7249dd09b4b6d481270cf
Svetlana <abramenkova.s.a@gmail.com>
2015-03-04 09:11:12 +0300

    web service

---
+++ WeatherStore/src/com/tia/weatherStorePack/CityRecord.java
@@ -0,0 +1,6 @@
+package com.tia.weatherStorePack;
+
+public class CityRecord {
+	public String name;
+	public String country;
+}
---
+++ WeatherStore/src/com/tia/weatherStorePack/JsonStrings.java
@@ -0,0 +1,7 @@
+package com.tia.weatherStorePack;
+
+public class JsonStrings {
+	public final static String JSON_TAG_CITIES = "cities";
+	public final static String JSON_TAG_CITY_NAME = "name";
+	public final static String JSON_TAG_COUNTRY = "country";
+}
